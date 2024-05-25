d762a0032bdc2f7c9c579ab9f0ddaf7368529271
yugandhardevops <134033216+yugandhardevops@users.noreply.github.com>
2024-05-25 22:50:28 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-echo "This is an Samples Script from GitHub"
+echo "This is an Sample Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
